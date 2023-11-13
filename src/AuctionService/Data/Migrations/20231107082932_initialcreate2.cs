369c54311dc9324db94600b1d7acdb01cbda1f65
unknown <gaurav.moolani@gmail.com>
2023-11-13 10:40:29 +0530

    section2

---
+++ src/AuctionService/Data/Migrations/20231107082932_initialcreate2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialcreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Items_AuctionId",
+                table: "Items");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Auctions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_AuctionId",
+                table: "Items",
+                column: "AuctionId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Items_AuctionId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Auctions");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_AuctionId",
+                table: "Items",
+                column: "AuctionId");
+        }
+    }
+}
