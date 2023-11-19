17ef00fb97ccde23f7dd0a7ca923d803514fa1ec
unknown <gaurav.moolani@gmail.com>
2023-11-19 22:14:48 +0530

    added search service

---
+++ src/SearchService/Data/DbInitilizer.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchService;
+
+public class DbInitilizer
+{
+    public static async Task InitDb(WebApplication app)
+    {
+        await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+        await DB.Index<Item>()
+        .Key(x => x.Make, KeyType.Text)
+        .Key(x => x.Model, KeyType.Text)
+        .Key(x => x.Color, KeyType.Text)
+        .CreateAsync();
+
+
+        var count = await DB.CountAsync<Item>();
+        using var scope = app.Services.CreateScope();
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionServiceHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+        Console.WriteLine(items.Count + "returned from auction service");
+        if (items.Count > 0) await DB.SaveAsync(items);
+
+
+
+        // if (count == 0)
+        // {
+        //     var itemadta = await File.ReadAllTextAsync("Data/auctions.json");
+        //     var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        //     var items = JsonSerializer.Deserialize<List<Item>>(itemadta, options);
+        //     await DB.SaveAsync(items);
+        // }
+    }
+
+}
